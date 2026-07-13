73c7675aa6d44620f93bc243f897e0a38db36b6f
Julian Francis <jgfrancis3@gmail.com>
2026-07-13 15:36:46 -0400

    Meta-mass

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  # your code here
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      self.class.attr_accessor(name) unless self.class.method_defined?(name)
+      public_send("#{name}=", value)
+    end
+  end
 end
